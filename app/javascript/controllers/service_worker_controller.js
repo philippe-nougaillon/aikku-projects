3a278f0641c055d73d3aebcbd61064621f7404c6
NOUGAILLON Philippe <philippe.nougaillon@gmail.com>
2026-09-16 10:28:11 +0200

    agy: "Uncaught (in promise) Error: Access to storage is not allowed from this context." fix

--- app/javascript/controllers/service_worker_controller.js
+++ app/javascript/controllers/service_worker_controller.js
@@ -1,7 +1,12 @@
 import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   connect() {
-    if (navigator.serviceWorker) {
+    if (!window.isSecureContext) {
+      console.info("[ServiceWorker] Secure context required; skipping registration.");
+      return;
+    }
+
+    if ("serviceWorker" in navigator) {
       if (navigator.serviceWorker.controller) {
         // If the service worker is already running, skip to state change
         this.stateChange();
@@ -9,10 +14,14 @@ export default class extends Controller {
         // Register the service worker, and wait for it to become active
         navigator.serviceWorker
           .register("/service-worker.js", { scope: "./" })
-          .then(function (reg) {
+          .then((reg) => {
             console.log("[Companion]", "Service worker registered!");
             console.log(reg);
+          })
+          .catch((error) => {
+            console.warn("[Companion] Service worker registration failed:", error);
           });
+
         navigator.serviceWorker.addEventListener(
           "controllerchange",
           this.controllerChange.bind(this)
@@ -22,10 +31,12 @@ export default class extends Controller {
   }
 
   controllerChange(event) {
-    navigator.serviceWorker.controller.addEventListener(
-      "statechange",
-      this.stateChange.bind(this)
-    );
+    if (navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.addEventListener(
+        "statechange",
+        this.stateChange.bind(this)
+      );
+    }
   }
 
   stateChange() {
